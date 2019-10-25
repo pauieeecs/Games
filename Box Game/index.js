40030e55af6ed7a6aa1dfe8140d3e15584fa9a01
Enes koç <32553624+eneskocc@users.noreply.github.com>
2019-10-25 15:58:18 +0300

    general refactor

--- Box Game/index.js
+++ Box Game/index.js
@@ -44,10 +44,10 @@ function changeCells1(x, y) {
     for(let i = 0; i<4; i++) {
         for(let j = 0;j < 4; j++) {
             if(x === i || y === j) {
-                if(cellxy1[i][j].style.backgroundColor ===  "rgba(255, 167, 38, .7)") {
+                if(cellxy1[i][j].style.backgroundColor ===  "rgba(255, 167, 38, 0.7)") {
                     cellxy1[i][j].style.backgroundColor = "transparent";
                 } else {
-                    cellxy1[i][j].style.backgroundColor = "rgba(255, 167, 38, .7)" ;
+                    cellxy1[i][j].style.backgroundColor = "rgba(255, 167, 38, 0.7)" ;
                 }
             }
         }
@@ -77,10 +77,10 @@ function changeCells(x, y) {
     for(let i = 0; i<4; i++) {
         for(let j = 0;j < 4; j++) {
             if(x === i || y === j) {
-                if(cellxy[i][j].style.backgroundColor ===  "rgba(255, 167, 38, .7)") {
+                if(cellxy[i][j].style.backgroundColor ===  "rgba(255, 167, 38, 0.7)") {
                     cellxy[i][j].style.backgroundColor = "transparent";
                 } else {
-                    cellxy[i][j].style.backgroundColor = "rgba(255, 167, 38, .7)" ;
+                    cellxy[i][j].style.backgroundColor = "rgba(255, 167, 38, 0.7)" ;
                 }
             }
         }
